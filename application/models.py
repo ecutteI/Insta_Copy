7b3a4009f88cba0a009451f50fd70e0cc075fdc6
EcutteI <zakatsfadenty@gmail.com>
2023-11-03 14:03:55 +0700

    second commit

--- application/models.py
+++ application/models.py
@@ -4,59 +4,57 @@ from application import db
 from datetime import datetime
 
 class User(db.Model, UserMixin):
-
-    __tablename__ = "users"
-    id                = db.Column(db.Integer, primary_key  = True)
-    status            = db.Column(db.Boolean, default      = True)
-    username          = db.Column(db.Text(128), nullable   = False)
-    fullname          = db.Column(db.Text(128), nullale    = False)
-    profile_photo_url = db.Column(db.String(255), default = "default.jpg")
-    bio               = db.Column(db.String(128))
-    created_date      = db.Column(db.DateTime(), default   = datetime.utcnow)
-    email             = db.Column(db.Text(64), nullable = False)
-    following_users   = db.relationship("Relation", foreign_keys = "Relation.ig_following", backref = "following", lazy=True)
-    follower_users    = db.relationship("Relation", foreign_keys = "Relation.ig_follower", backref = "follower", lazy=True)
-    posts             = db.relationship("Post", backref = "posts_owner", lazy = True)
-    likes             = db.relationship("Like", backref = "likes_owner", lazy = True)
-    comments          = db.relationship("Comment", backref = "comments_owner", lazy = True)
-
+    __tablename__   = "users"
+    id              = db.Column(db.Integer, primary_key = True)
+    username        = db.Column(db.String(128), nullable = False)
+    password        = db.Column(db.String(128), nullable = False)
+    fullname        = db.Column(db.String(128), nullable = False)
+    email           = db.Column(db.String(128), nullable = False)
+    profile_pic     = db.Column(db.String(128), default="default.jpg")
+    bio             = db.Column(db.String(128))
+    join_date       = db.Column(db.DateTime, default=datetime.utcnow)
+    status          = db.Column(db.Boolean, default=True)
+    following_users = db.relationship("Relation", foreign_keys="Relation.id_following", backref="following", lazy=True)
+    follower_users  = db.relationship("Relation", foreign_keys="Relation.id_follower", backref="follower", lazy=True)
+    posts           = db.relationship("Post", backref="posts_owner", lazy=True)
+    comments        = db.relationship("Comment", backref="comments_owner", lazy=True)
+    likes           = db.relationship("Like", backref="likes_owner", lazy=True)
 
 class Relation(db.Model):
-
-    __tablename__ = "relations"
-    id            = db.Column(db.Integer, primary_key = True)
-    follower_id   = db.Column(db.Interger, db.ForeignKey("users.id"), nullable = False)
-    following_id  = db.Column(db.Interger, db.ForeignKey("users.id"), nullable = False)
-    status        = db.Column(db.Boolean, default     = True)
-    relation_date = db.Column(db.DateTime, nullable  = False)
+    __tablename__   = "relations"
+    id              = db.Column(db.Integer, primary_key = True)
+    id_follower     = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    id_following    = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    status          = db.Column(db.Boolean, default=True)
+    relation_date   = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Post(db.Model):
+    __tablename__   = "posts"
+    id              = db.Column(db.Integer, primary_key = True)
+    author_id       = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    photo           = db.Column(db.String(128), nullable = False)
+    caption         = db.Column(db.String(128), default="")
+    status          = db.Column(db.Boolean, default=True)
+    post_date       = db.Column(db.DateTime, default=datetime.utcnow)
+    comments        = db.relationship("Comment", backref="commented", lazy=True)
+    likes           = db.relationship("Like", backref="liked", lazy=True)
 
-    __tablename__ = "posts"
-    id           = db.Column(db.Integer, primary_key  = True)
-    user_id      = db.Column(db.Interger, db.ForeignKey("users.id"), nullable = False)
-    image_url    = db.Column(db.String(255), nullable = False)
-    caption      = db.Column(db.Text, nullable        = False)
-    post_date    = db.Column(db.DateTime, nullable    = False)
-    status       = db.Column(db.Boolean, default      = True)
-    comments     = db.relationship("Comment", backref = "commented", lazy = True)
-    likes        = db.relationship("Like", backref = "liked", lazy = True)
 
-class Comment(db.Model): 
-
-    __tablename__ = "comments"
-    id            = db.Column(db.Interger, primary_key=True)
-    user_id       = db.Column(db.Interger, db.ForeignKey("users.id"), nullable=False)
-    content       = db.Column(db.Text, nullable=False)
-    comment_date  = db.Column(db.DateTime, default=datetime.utcnow)
-    post_id       = db.Column(db.Interger, db.ForeignKey("posts.id"))
-    status        = db.Column(db.Boolean, default      = True) 
+class Comment(db.Model):
+    __tablename__   = "comments"
+    id              = db.Column(db.Integer, primary_key = True)
+    text            = db.Column(db.Text, nullable = False)
+    commenter_id    = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    post_id         = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+    hidden          = db.Column(db.Boolean, default=False)
+    comment_date    = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Like(db.Model):
+    __tablename__   = "likes"
+    id              = db.Column(db.Integer, primary_key = True)
+    user_id         = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    post_id         = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable = False)
+    status          = db.Column(db.Boolean, default=True)
+    like_date       = db.Column(db.DateTime, default=datetime.utcnow)
 
-    __tablename__ = "likes"
-    id            = db.Column(db.Interger, primary_key=True)
-    user_id       = db.Column(db.Interger, db.ForeignKey("users.id"), nullable=False)
-    date          = db.Column(db.DateTime, nullable=False)
-    post_id       = db.Column(db.Interger, db.ForeignKey("posts.id"), nullable=False)
-    created_date  = db.Column(db.DateTime, nullable=False)
+print("sex")
--- application/utils.py
+++ application/utils.py
@@ -1,9 +1,15 @@
+import os
+import secrets
+from PIL import Image
+
+from flask import current_app
+
 from wtforms.validators import ValidationError
 
 from application import login_manager
 from application.models import User
 
-# FORM UTILS
+#FORM UTILS
 def exists_email(form, email):
     user = User.query.filter_by(email = email.data).first()
     if user:
@@ -11,15 +17,37 @@ def exists_email(form, email):
     
 def not_exists_email(form, email):
     user = User.query.filter_by(email = email.data).first()
-    if user:
+    if not user:
         raise ValidationError("Email not found.")
-    
+
 def exists_username(form, username):
     user = User.query.filter_by(username = username.data).first()
     if user:
-        raise ValidationError("Username already exists. Please use a different username.")
+        raise ValidationError("Username already exists. Please use a different username")
+#END OF FORM UTILS
 
-# LOGIN MANAGER UTILS
+
+#LOGIN MANAGER UTILS
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+#END OF LOGIN MANAGER UTILS
+
+
+#IMAGE SAVE UTILS
+
+def save_image(form_picture_data):
+    random_hex = secrets.token_hex(5)
+    f_name, f_ext = os.path.splitext(form_picture_data.filename)
+    picture_fn = 'images/posts/'+random_hex+f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/', picture_fn)
+
+    image = Image.open(form_picture_data)
+    # i_width, i_height = image.size
+    # ratio = i_width/1000
+    # output_size = (i_width/ratio, i_height/ratio)
+    # image.thumbnail(image)
+
+    image.save(picture_path)
+
+    return picture_fn
